2e3cc23c4e8acd0fac1f7ef2f7aa314aeb96ef46
Simichus <simonefilippobilardo@gmail.com>
2021-08-26 15:12:13 +0200

    Completata sezione chats-bar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+console.log(Vue);
+
 Vue.config.devtools = true;
+
+const app = new Vue({
+  el: "#app",
+  data: {
+    user: user,
+    contacts: contacts,
+  },
+});
--- js/users.js
+++ js/users.js
@@ -1,22 +1,22 @@
 const user = {
-  name: 'Nome Utente',
-  avatar: '_io',
+  name: "Nome Utente",
+  avatar: "_io",
 };
 const contacts = [
   {
-    name: 'Michele',
-    avatar: '_1',
+    name: "Michele",
+    avatar: "_1",
   },
   {
-    name: 'Fabio',
-    avatar: '_2',
+    name: "Fabio",
+    avatar: "_2",
   },
   {
-    name: 'Samuele',
-    avatar: '_3',
+    name: "Samuele",
+    avatar: "_3",
   },
   {
-    name: 'Luisa',
-    avatar: '_4',
+    name: "Alberto",
+    avatar: "_4",
   },
 ];

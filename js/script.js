e1c43f69a146f9d89a924f145ad08c7b7ecfaacc
Simichus <simonefilippobilardo@gmail.com>
2021-08-28 09:01:19 +0200

    Aggiunta funzione cambio chat

--- js/script.js
+++ js/script.js
@@ -8,5 +8,39 @@ const app = new Vue({
     user: user,
     contacts: contacts,
     messageToSend: "",
+    filter: "",
+    currentFiltered: contacts,
+    activeChatName: "",
+    activeChatAvatar: "",
+  },
+  methods: {
+    chatsFiltered(filter) {
+      if (!filter.trim()) {
+        this.currentFiltered = this.contacts;
+      } else {
+        this.currentFiltered = [];
+        this.contacts.forEach((contact) => {
+          if (contact.name.contains(filter)) {
+            this.currentFiltered.push(contact);
+          }
+        });
+      }
+    },
+    activeChat(index) {
+      this.contacts.forEach((contact) => {
+        contact.visible = false;
+      });
+      this.contacts[index].visible = true;
+      this.activeChatName = this.contacts[index].name;
+      this.activeChatAvatar = this.contacts[index].avatar;
+    },
+  },
+  created() {
+    this.contacts.forEach((contact) => {
+      contact.visible = false;
+    });
+    this.contacts[0].visible = true;
+    this.activeChatName = this.contacts[0].name;
+    this.activeChatAvatar = this.contacts[0].avatar;
   },
 });
